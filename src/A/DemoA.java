70ae57e9d8afe75a755aca0024cd59ce376b694d
Cree <Cree@cristians-air.attlocal.net>
2024-01-04 15:16:34 -0800

    NEW

---
+++ src/A/DemoA.java
@@ -0,0 +1,18 @@
+package A;
+
+public class DemoA {
+
+	public static void main(String[] args) {
+		System.out.println("Monday");
+		
+		System.out.println("STUFF");
+		
+		
+		System.out.println("MORE Stuff");
+	}
+	public static void add(int d, int b) {
+		System.out.println(d + b);
+		
+		
+	}
+}
