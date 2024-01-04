bd9f6ff4a8a3d779c1a150540d1894d6fe339f01
Cree <Cree@cristians-air.attlocal.net>
2024-01-04 14:58:59 -0800

    More changes

--- src/A/DemoA.java
+++ src/A/DemoA.java
@@ -4,7 +4,12 @@ public class DemoA {
 
 	public static void main(String[] args) {
 		System.out.println("Monday");
-		
+
 		System.out.println("UPDATE!");
+	}
+
+	// Sprint 2 automation
+	public static void add(int a, int b) {
+		System.out.println(a + b);
 	}
 }
